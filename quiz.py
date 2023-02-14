3ef8d3eede9d707582af4dccd16138726a040cb9
Samuel Máximo <samuelspam6@gmail.com>
2023-02-14 15:05:48 -0300

    Adicionado listas de perguntas

--- quiz.py
+++ quiz.py
@@ -53,3 +53,36 @@ def questionario(questao, lista_perguntas):
     
     # Retorna a função respostas
     return respostas
+
+perguntas = [
+    {
+        'Pergunta' : 'Quanto é 2+2?',
+        'Opções' : ['1','3','4','5','6'],
+        'Resposta' : '4'
+    },
+    {
+        'Pergunta' : 'Quanto é 5 * 5?',
+        'Opções' : ['5','255','55','25','45'],
+        'Resposta' : '25'
+    },
+    {
+        'Pergunta' : 'Quanto é 10/2?',
+        'Opções' : ['4','3','5','1','2'],
+        'Resposta' : '5'
+    },
+]
+
+numero_questao = 1
+i=0
+for pergunta in perguntas:
+    
+    print(f'{numero_questao}.Pergunta',perguntas[i]['Pergunta'])
+    print('Opções:', perguntas[i]['Opções'])
+    numero_questao += 1
+    i += 1
+    
+questao = int(input('Digite qual das questões você quer responder: '))
+seleciona_questao = questionario(questao, perguntas)
+resposta_usuario = int(input('Resposta: '))
+
+seleciona_questao(resposta_usuario)
